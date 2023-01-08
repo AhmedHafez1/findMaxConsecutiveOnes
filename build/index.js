e6579c22ba8bc08c90f679513ee17677ba5f8b1d
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-01-08 13:47:15 +0200

    first commit

---
+++ build/index.js
@@ -0,0 +1,23 @@
+"use strict";
+function findMaxConsecutiveOnes(nums) {
+    let maxConsOnes = 0;
+    let currentConsOnes = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1)
+            currentConsOnes++;
+        else {
+            if (currentConsOnes > maxConsOnes) {
+                maxConsOnes = currentConsOnes;
+            }
+            currentConsOnes = 0;
+        }
+        if (currentConsOnes > maxConsOnes) {
+            maxConsOnes = currentConsOnes;
+        }
+    }
+    return maxConsOnes;
+}
+let nums = [1, 1, 0, 1, 1, 1];
+console.log(findMaxConsecutiveOnes(nums));
+nums = [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0];
+console.log(findMaxConsecutiveOnes(nums));
